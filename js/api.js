227b81f258041f281ca0d3bda12ffeaf991ab600
russcuevas <russelcuevas0@gmail.com>
2023-10-15 13:56:34 +0800

    api

--- js/api.js
+++ js/api.js
@@ -11,42 +11,46 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(data);
             const recipeList = document.getElementById("recipe-list");
 
+            let recipeCount = 0;
             data.forEach(function (recipe) {
-                const recipeDiv = document.createElement("div");
-                recipeDiv.classList.add("recipe");
+                if (recipeCount < 5 && recipe.description && recipe.mainIngredient && recipe.photoUrl) {
+                    const recipeDiv = document.createElement("div");
+                    recipeDiv.classList.add("recipe");
 
-                const recipeTitle = document.createElement("h2");
-                recipeTitle.textContent = "Title: " + recipe.title;
-                recipeDiv.appendChild(recipeTitle);
+                    const recipeTitle = document.createElement("h2");
+                    recipeTitle.textContent = "Title: " + recipe.title;
+                    recipeDiv.appendChild(recipeTitle);
 
-                const recipeImage = document.createElement("img");
-                recipeImage.src = recipe.photoUrl;
-                recipeImage.alt = recipe.title;
-                recipeImage.style.width = "200px";
-                recipeImage.style.height = "200px";
-                recipeImage.style.borderRadius = "50px";
-                recipeDiv.appendChild(recipeImage);
+                    const recipeImage = document.createElement("img");
+                    recipeImage.src = recipe.photoUrl;
+                    recipeImage.alt = recipe.title;
+                    recipeImage.style.width = "200px";
+                    recipeImage.style.height = "200px";
+                    recipeImage.style.borderRadius = "50px";
+                    recipeDiv.appendChild(recipeImage);
 
-                const recipeDescription = document.createElement("p");
-                recipeDescription.textContent = "Description: " + recipe.description;
-                recipeDiv.appendChild(recipeDescription);
+                    const recipeDescription = document.createElement("p");
+                    recipeDescription.textContent = "Description: " + recipe.description;
+                    recipeDiv.appendChild(recipeDescription);
 
-                const recipeMainIngredient = document.createElement("p");
-                recipeMainIngredient.textContent = "Main Ingredients: " + recipe.mainIngredient;
-                recipeDiv.appendChild(recipeMainIngredient);
+                    const recipeMainIngredient = document.createElement("p");
+                    recipeMainIngredient.textContent = "Main Ingredients: " + recipe.mainIngredient;
+                    recipeDiv.appendChild(recipeMainIngredient);
 
-                const recipeIngredients = document.createElement("p");
-                recipeIngredients.textContent = "Ingredients: " + recipe.ingredients;
-                recipeDiv.appendChild(recipeIngredients);
+                    const recipeIngredients = document.createElement("p");
+                    recipeIngredients.textContent = "Ingredients: " + recipe.ingredients;
+                    recipeDiv.appendChild(recipeIngredients);
 
-                const recipeCourse = document.createElement("p");
-                recipeCourse.textContent = "Course: " + recipe.course;
-                recipeDiv.appendChild(recipeCourse);
+                    const recipeCourse = document.createElement("p");
+                    recipeCourse.textContent = "Course: " + recipe.course;
+                    recipeDiv.appendChild(recipeCourse);
 
-                recipeList.appendChild(recipeDiv);
+                    recipeList.appendChild(recipeDiv);
+                    recipeCount++;
+                }
             });
         })
         .catch(function (error) {
-            console.error("Error fetching data:", error);
+            console.error("Error fetching object:", error);
         });
 });
